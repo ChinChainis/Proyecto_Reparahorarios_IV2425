cd4e125f0752b9a619296ab4860e1d0b98475448
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2025-01-16 21:59:16 +0100

    #28 función dentro de entidad de horario para asegurar no repeticion
    Una misma asignatura no debería estar presente por la mañana y por la tarde a la vez, dado que podría tratarse que el proceso de selección ha escogido la misma asignatura de varios horarios distintos en un mismo resultado

--- models/horario.go
+++ models/horario.go
@@ -1,11 +1,23 @@
 package models
 
 type Horario struct {
-    Horario []FranjaHoraria
+	Horario []FranjaHoraria
 }
 
 func NewHorario() (*Horario, error) {
-    return &Horario{
-        Horario: []FranjaHoraria{}, 
-    }, nil
+	return &Horario{
+		Horario: []FranjaHoraria{},
+	}, nil
+}
+
+func AsignaturaRepetida(horario Horario) bool {
+	var hayRepeticion bool = false
+	for i := 0; i < len(horario.Horario); i++ {
+		for j := i + 1; j < len(horario.Horario); j++ {
+			if horario.Horario[i].Asignatura == horario.Horario[j].Asignatura && horario.Horario[i].Turno == horario.Horario[j].Turno {
+				hayRepeticion = true
+			}
+		}
+	}
+	return hayRepeticion
 }
