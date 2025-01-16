49240fbf6471e37f90b65aab6edd598e56e96722
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2025-01-16 22:46:18 +0100

    #28 descarte de criterio de testeo

--- models/horario.go
+++ models/horario.go
@@ -1,28 +1,11 @@
 package models
 
-import "fmt"
-
 type Horario struct {
 	Horario []FranjaHoraria
 }
 
-func NewHorario(horario []FranjaHoraria) (*Horario, error) {
-	if !AsignaturaRepetida(horario) {
-		return nil, fmt.Errorf("Error de lógica interna de las asignaturas")
-	}
+func NewHorario() (*Horario, error) {
 	return &Horario{
-		Horario: horario,
+		Horario: []FranjaHoraria{},
 	}, nil
-}
-
-func AsignaturaRepetida(horario []FranjaHoraria) bool {
-	var noHayRepeticion bool = true
-	for i := 0; i < len(horario); i++ {
-		for j := i + 1; j < len(horario); j++ {
-			if horario[i].Asignatura == horario[j].Asignatura && horario[i].Turno == horario[j].Turno {
-				noHayRepeticion = false
-			}
-		}
-	}
-	return noHayRepeticion
 }
