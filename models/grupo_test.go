6796b8c0d78b1da53a5f0b7a619f56a3636c56c1
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2025-01-17 23:28:13 +0100

    #31 creación de función de testeo sobre función de superposición

--- models/grupo_test.go
+++ models/grupo_test.go
@@ -2,6 +2,20 @@ package models
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestNoSuperposicionDeAsignaturasEnGrupo(t *testing.T) {}
+func TestNoSuperposicionDeAsignaturasEnGrupo(t *testing.T) {
+	asignaturasTesteo := []struct {
+		franjas   []FranjaHoraria
+		resultado bool
+	}{
+		{franjas: []FranjaHoraria{{Lunes, HoraMinuto{9, MinutoCero}, UnaHora, "fisica", Manana},
+			{Lunes, HoraMinuto{10, MinutoCero}, DosHoras, "fisica", Manana}}, resultado: true},
+	}
+	for _, tt := range asignaturasTesteo {
+		err := NoSuperposicionDeAsignaturasEnGrupo(tt.franjas)
+		assert.Equal(t, err, tt.resultado, "Los resultados deberían ser iguales")
+	}
+}
