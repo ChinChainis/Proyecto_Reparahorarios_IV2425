628b85509d7d21bb26ee7cfc0e8a734f07485ecd
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2025-01-17 23:39:05 +0100

    #31 especialización de los test de función de superposición
    Para el caso de que una asignatura pise un poco el horario de otra pero no completamente

--- models/grupo_test.go
+++ models/grupo_test.go
@@ -19,3 +19,17 @@ func TestSuperposicionHorasAsignaturaCompletamenteSuperpuesta(t *testing.T) {
 		assert.Equal(t, err, tt.resultado, "Los resultados deberían ser iguales")
 	}
 }
+
+func TestSuperposicionHorasAsignaturaParcialmenteSuperpuesta(t *testing.T) {
+	asignaturasTesteo := []struct {
+		franjas   []FranjaHoraria
+		resultado bool
+	}{
+		{franjas: []FranjaHoraria{{Lunes, HoraMinuto{12, MinutoCero}, DosHoras, "fisica", Manana},
+			{Lunes, HoraMinuto{11, MinutoCero}, DosHoras, "lengua", Manana}}, resultado: true},
+	}
+	for _, tt := range asignaturasTesteo {
+		err := NoSuperposicionDeAsignaturasEnGrupo(tt.franjas)
+		assert.Equal(t, err, tt.resultado, "Los resultados deberían ser iguales")
+	}
+}
