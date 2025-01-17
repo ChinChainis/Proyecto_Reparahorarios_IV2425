fe6c1493c772a30cbde9306b082b351e190e0ade
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2025-01-17 22:24:05 +0100

    #31 añadida función a grupo.go para comprobar estado de superposición entre horarios de asignaturas según su hora de inicio

--- models/grupo.go
+++ models/grupo.go
@@ -16,6 +16,30 @@ func NewGrupo(nombre string) (*Grupo, error) {
 
 		Nombre:  nombre,
 		Horario: horario,
-		
 	}, nil
+}
+
+func NoSuperposicionDeAsignaturasEnGrupo(grupoEntrada Grupo) bool {
+	var haySuperposicion bool = false
+	for i := 0; i < len(grupoEntrada.Horario.Horario); i++ {
+		var franjaActual FranjaHoraria = grupoEntrada.Horario.Horario[i]
+
+		for j := i + 1; j < len(grupoEntrada.Horario.Horario); j++ {
+			var franjaAComparar FranjaHoraria = grupoEntrada.Horario.Horario[j]
+			if franjaActual.Dia == franjaAComparar.Dia {
+				if franjaActual.Inicio.Hora == franjaAComparar.Inicio.Hora {
+					haySuperposicion = true
+				} else if franjaActual.Inicio.Hora < franjaAComparar.Inicio.Hora &&
+					(franjaActual.Inicio.Hora+int(franjaActual.Duracion)) > franjaAComparar.Inicio.Hora {
+					haySuperposicion = true
+				} else if franjaActual.Inicio.Hora > franjaAComparar.Inicio.Hora &&
+					franjaActual.Inicio.Hora < (franjaAComparar.Inicio.Hora+int(franjaAComparar.Duracion)) {
+					haySuperposicion = true
+				}
+			}
+
+		}
+
+	}
+	return haySuperposicion
 }
