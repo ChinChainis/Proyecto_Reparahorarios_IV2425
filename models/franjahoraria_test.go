b4103a71034f7a7391630dba7fb9bd8c7d97f0e6
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2025-01-17 18:16:29 +0100

    #27 añadido test para asegurar límites del horario

--- models/franjahoraria_test.go
+++ models/franjahoraria_test.go
@@ -23,7 +23,37 @@ func TestCorrespondenciaHoraTurno(t *testing.T) {
 			tt.hora,
 			tt.turno,
 		)
-		assert.Equal(t, err, tt.resultado, "Los resultados debería ser iguales")
+		assert.Equal(t, err, tt.resultado, "Los resultados deberían ser iguales")
+	}
+
+}
+
+func TestDuracionTurnoCorrecto(t *testing.T) {
+	horaTesteo := []struct {
+		hora  int
+		turno Turno
+	}{
+		{0, Manana},
+		{1, Manana},
+		{2, Manana},
+		{3, Manana},
+		{4, Manana},
+		{5, Manana},
+		{6, Manana},
+		{7, Manana},
+		{14, Manana},
+		{14, Tarde},
+		{21, Tarde},
+		{22, Tarde},
+		{23, Tarde},
+	}
+
+	for _, tt := range horaTesteo {
+		err := CompruebaCorrespondenciaHoraTurno(
+			tt.hora,
+			tt.turno,
+		)
+		assert.Equal(t, err, false, "Los resultados deben ser falsos")
 	}
 
 }
