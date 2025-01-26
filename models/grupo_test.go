c25e5daba5fdf1f6104367739c3f15cbbe120c18
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2025-01-26 12:54:13 +0100

    #34 función de testeo de un horario por día funcional

--- models/grupo_test.go
+++ models/grupo_test.go
@@ -21,3 +21,21 @@ func TestSuperposicionHoras(t *testing.T) {
 		assert.NoError(t, err)
 	}
 }
+
+func TestGeneracionHorario(t *testing.T) {
+	franjasTesteo := []struct {
+		franjas []FranjaHoraria
+		dia     DiaSemana
+	}{
+		{franjas: []FranjaHoraria{{Lunes, HoraMinuto{8, MinutoCero}, DosHoras, "fisica", Manana},
+			{Lunes, HoraMinuto{12, MinutoCero}, DosHoras, "mates", Manana},
+			{Martes, HoraMinuto{8, MinutoCero}, DosHoras, "lengua", Manana},
+			{Martes, HoraMinuto{10, MinutoCero}, DosHoras, "mates", Manana},
+			{Martes, HoraMinuto{16, MinutoCero}, UnaHora, "fisica", Tarde}}, dia: Martes},
+	}
+	for _, tt := range franjasTesteo {
+		res, err := haceHorario(tt.franjas, tt.dia)
+		assert.Equal(t, len(res), 3, "horario esperado")
+		assert.NoError(t, err)
+	}
+}
